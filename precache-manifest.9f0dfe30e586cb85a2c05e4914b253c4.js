574900a2eacf3c910d505ef5179375f56a8e54d6
Saul Martínez <saul.martinez05@gmail.com>
2019-10-31 22:34:37 -0600

    Updates

---
+++ precache-manifest.9f0dfe30e586cb85a2c05e4914b253c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4caff410757d85052039fac2976b26",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3afb468d4f8342e6ea35",
+    "url": "/monsters-rolodex/static/css/main.3ec93a8c.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "3afb468d4f8342e6ea35",
+    "url": "/monsters-rolodex/static/js/main.e43ef5f7.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
